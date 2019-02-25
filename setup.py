62b1e087c44ce6e2d7ec08cf8e147ff75fd76f57
Mariusz Pasek <mariusz.pasek@aeolusbot.com>
2019-02-26 00:57:12 +0100

    removing requirements.txt file

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ import setuptools
 import os
 
 
-def dependencies():
-    requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
-    return open(requirements_path, 'r').read().splitlines()
-
-
 def readme():
     this_directory = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -17,14 +12,14 @@ def readme():
 
 setuptools.setup(
     name="app_executor",
-    version='1.0.1',
+    version='1.0.3',
     author="Mariusz Pasek",
     author_email="pasiasty@gmail.com",
     description="Tool for safe launching external processes",
     entry_points={"pytest11": ["app_executor = app_executor.conftest"]},
     url="https://github.com/pasiasty/app-executor",
     packages=setuptools.find_packages(),
-    install_requires=dependencies(),
+    install_requires=['pytest'],
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
