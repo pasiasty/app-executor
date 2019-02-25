41af3429aab7a8f33541a5c71ebcd5cea67a20be
Mariusz Pasek <mariusz.pasek@aeolusbot.com>
2019-02-25 12:31:52 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="app_executor",
+    version='1.0.0',
+    author="Mariusz Pasek",
+    author_email="pasiasty@gmail.com",
+    description="Tool for safe launching external processes",
+    url="EMPTY",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
